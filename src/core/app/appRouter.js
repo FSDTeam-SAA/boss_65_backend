b9a9012b4b20f767c8892e7c1aa963361e53b412
jarifMohammed <mohammedjarif@protonmail.com>
2025-05-12 10:51:48 +0600

    Remove user services functionality and associated routes

--- src/core/app/appRouter.js
+++ src/core/app/appRouter.js
@@ -4,7 +4,7 @@ import authRoutes from '../../entities/auth/auth.routes.js';
 import userRoutes from '../../entities/user/user.routes.js';
 import contactRoutes from '../../entities/contact/contact.routes.js';
 import newsletterSubscriptionRoutes from '../../entities/newsletterSubscription/newsletterSubscription.routes.js'
-import userServicesRoutes from '../../entities/userServices/userServices.routes.js'
+
 import reviewsRoutes from '../../entities/review/review.routes.js'
 import { getStats } from '../../lib/statController.js';
 
@@ -17,7 +17,7 @@ router.use('/v1/auth', authRoutes);
 router.use('/v1/user', userRoutes);
 router.use('/v1/contact', contactRoutes)
 router.use('/v1/newsletterSubscription', newsletterSubscriptionRoutes)
-router.use('/v1/userServices', userServicesRoutes)
+
 router.use('/v1/reviews', reviewsRoutes)
 router.use('/v1/stats', getStats)
 
