ff707579da925310ab17b5b3ccc5e79a7ea63cf5
jarifMohammed <mohammedjarif@protonmail.com>
2025-05-15 11:02:12 +0600

    Initiated filmsa14

--- src/core/app/appRouter.js
+++ src/core/app/appRouter.js
@@ -5,9 +5,7 @@ import userRoutes from '../../entities/user/user.routes.js';
 import contactRoutes from '../../entities/contact/contact.routes.js';
 import newsletterSubscriptionRoutes from '../../entities/newsletterSubscription/newsletterSubscription.routes.js'
 import reviewsRoutes from '../../entities/review/review.routes.js'
-import applicationRoutes from '../../entities/application/application.routes.js'
-import lenderRoutes from '../../entities/lender/Listings/lisitngs.routes.js'
-import adminListingRoutes from '../../entities/admin/Lisitngs/ReviewandMain Site Listing/adminListing.routes.js'
+
 
 const router = express.Router();
 
@@ -18,9 +16,6 @@ router.use('/v1/user', userRoutes);
 router.use('/v1/contact', contactRoutes)
 router.use('/v1/newsletterSubscription', newsletterSubscriptionRoutes)
 router.use('/v1/reviews', reviewsRoutes)
-router.use('/v1/application', applicationRoutes)
-router.use('/v1/lender', lenderRoutes)
-router.use('/v1/admin',adminListingRoutes)
 
 
 
