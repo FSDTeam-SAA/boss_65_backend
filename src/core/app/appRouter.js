ab5323632492801b44729a70512f909027a3daef
jishad10 <jishadjishad49@gmail.com>
2025-05-16 09:18:43 +0600

    some changes

--- src/core/app/appRouter.js
+++ src/core/app/appRouter.js
@@ -1,22 +1,15 @@
 import express from 'express';
-import testRoutes from '../../entities/test/b.routes.js';
 import authRoutes from '../../entities/auth/auth.routes.js';
 import userRoutes from '../../entities/user/user.routes.js';
 import contactRoutes from '../../entities/contact/contact.routes.js';
-import newsletterSubscriptionRoutes from '../../entities/newsletterSubscription/newsletterSubscription.routes.js'
-import reviewsRoutes from '../../entities/review/review.routes.js'
 
 
 const router = express.Router();
 
 // Define all your routes here
-router.use('/v1/tests', testRoutes);
 router.use('/v1/auth', authRoutes);
 router.use('/v1/user', userRoutes);
 router.use('/v1/contact', contactRoutes)
-router.use('/v1/newsletterSubscription', newsletterSubscriptionRoutes)
-router.use('/v1/reviews', reviewsRoutes)
-
 
 
 
